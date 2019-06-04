4787207e5609379f8e39bb25abede1b3d679f80e
rushi444 <rushi44@gmail.com>
2019-06-04 14:41:48 -0700

    added search

--- instagram-app/src/components/SearchBar/SearchBar.jsx
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -3,28 +3,32 @@ import './SearchBar.css';
 import { IoLogoInstagram, IoIosHeartEmpty } from 'react-icons/io';
 import { FiCompass, FiUser } from 'react-icons/fi';
 
-const SearchBar = (props) => {
+const SearchBar = ({ handleSearch, search }) => {
     return (
         <header className="header">
-        <nav>
-        <div className="logoContainer">
-                <div className="logoIcon">
-                <IoLogoInstagram />
-                </div>
-                <h3 className="logoText">
-                    instagram
+            <nav>
+                <div className="logoContainer">
+                    <div className="logoIcon">
+                        <IoLogoInstagram />
+                    </div>
+                    <h3 className="logoText">
+                        instagram
                 </h3>
-            </div>
-            <input className="search" placeholder= "Search"/>
-            <div className="iconContainer">
-            <div><FiCompass /></div>
-            <div><IoIosHeartEmpty /></div>
-            <div><FiUser /></div>
-            </div>
-        </nav>
+                </div>
+                <input 
+                className="search" 
+                placeholder="Search" 
+                value={search} 
+                onChange={handleSearch} />
+                <div className="iconContainer">
+                    <div><FiCompass /></div>
+                    <div><IoIosHeartEmpty /></div>
+                    <div><FiUser /></div>
+                </div>
+            </nav>
 
-         </header>
+        </header>
     )
 }
 
- export default SearchBar;
+export default SearchBar;
